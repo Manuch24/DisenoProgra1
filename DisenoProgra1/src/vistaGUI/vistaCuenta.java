904995951f5d1d5589ceef468eebe61baa56df8d
Manuel <manuelchavesmoya@gmail.com>
2022-05-05 09:34:07 -0600

    Hice solo la interfaz de persona, falta conectarr

--- DisenoProgra1/src/vistaGUI/vistaCuenta.java
+++ DisenoProgra1/src/vistaGUI/vistaCuenta.java
@@ -1,48 +1,49 @@
 package vistaGUI;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
-import javax.swing.JComboBox;
-import java.awt.FlowLayout;
-import javax.swing.JTextField;
+import java.awt.Font;
 
-public class vistaCuenta {
+public class vistaCuenta extends JFrame {
 
-	private JFrame frame;
-	private JTextField txtMontoInicial;
-	
-	public vistaCuenta() {
-		initialize();
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					vistaCuenta frame = new vistaCuenta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Create the frame.
 	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 781, 498);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+	public vistaCuenta() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 543, 435);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Ingrese el PIN");
-		lblNewLabel.setBounds(69, 77, 77, 23);
-		frame.getContentPane().add(lblNewLabel);
-		
-		JTextField txtPIN = new JTextField();
-		txtPIN.setBounds(166, 79, 96, 19);
-		frame.getContentPane().add(txtPIN);
-		txtPIN.setColumns(10);
-		
-		JLabel txoDepositoInicial = new JLabel("Ingrese el deposito inicial");
-		txoDepositoInicial.setBounds(69, 135, 129, 23);
-		frame.getContentPane().add(txoDepositoInicial);
-		
-		txtMontoInicial = new JTextField();
-		txtMontoInicial.setBounds(208, 137, 96, 19);
-		frame.getContentPane().add(txtMontoInicial);
-		txtMontoInicial.setColumns(10);
+		JLabel lblNewLabel = new JLabel("Puto el que lo lea");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 38));
+		lblNewLabel.setBounds(116, 140, 322, 87);
+		contentPane.add(lblNewLabel);
 	}
+
 }
--- DisenoProgra1/src/vistaGUI/vistaPersona.java
+++ DisenoProgra1/src/vistaGUI/vistaPersona.java
@@ -1,12 +1,25 @@
 package vistaGUI;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import com.toedter.calendar.JDateChooser;
 
-public class vistaPersona {
+public class vistaPersona extends JFrame {
 
-	private JFrame frame;
+	private JPanel contentPane;
+	private JTextField txtCedula;
+	private JTextField txtNombre;
+	private JTextField txtPrimerApellid;
+	private JTextField txtSegundoApellido;
+	private JTextField txtNumTelefono;
+	private JTextField txtEmail;
 
 	/**
 	 * Launch the application.
@@ -15,8 +28,8 @@ public class vistaPersona {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					vistaPersona window = new vistaPersona();
-					window.frame.setVisible(true);
+					vistaPersona frame = new vistaPersona();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -25,17 +38,85 @@ public class vistaPersona {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public vistaPersona() {
-		initialize();
+		setTitle("Registrar Usuario");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 511, 476);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Registro de persona");
+		lblNewLabel.setBounds(10, 33, 152, 22);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Ingrese su c\u00E9dula:");
+		lblNewLabel_1.setBounds(49, 63, 135, 22);
+		contentPane.add(lblNewLabel_1);
+		
+		txtCedula = new JTextField();
+		txtCedula.setBounds(229, 64, 122, 20);
+		contentPane.add(txtCedula);
+		txtCedula.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Ingrese su nombre:");
+		lblNewLabel_2.setBounds(49, 114, 152, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		txtNombre = new JTextField();
+		txtNombre.setBounds(229, 111, 163, 20);
+		contentPane.add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		JLabel txtPrimerApellido = new JLabel("Ingrese su primer apellido:");
+		txtPrimerApellido.setBounds(46, 157, 163, 14);
+		contentPane.add(txtPrimerApellido);
+		
+		txtPrimerApellid = new JTextField();
+		txtPrimerApellid.setBounds(229, 154, 163, 20);
+		contentPane.add(txtPrimerApellid);
+		txtPrimerApellid.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Ingrese su segundo apellido:");
+		lblNewLabel_3.setBounds(46, 196, 163, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		txtSegundoApellido = new JTextField();
+		txtSegundoApellido.setBounds(229, 193, 163, 20);
+		contentPane.add(txtSegundoApellido);
+		txtSegundoApellido.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Ingrese fecha de nacimiento:");
+		lblNewLabel_4.setBounds(49, 237, 170, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Ingrese n\u00FAmero de tel\u00E9fono:");
+		lblNewLabel_5.setBounds(49, 278, 170, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		txtNumTelefono = new JTextField();
+		txtNumTelefono.setBounds(229, 275, 163, 20);
+		contentPane.add(txtNumTelefono);
+		txtNumTelefono.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("Ingrese su correo:");
+		lblNewLabel_6.setBounds(49, 315, 135, 14);
+		contentPane.add(lblNewLabel_6);
+		
+		txtEmail = new JTextField();
+		txtEmail.setBounds(229, 312, 240, 20);
+		contentPane.add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		JButton btnRegistrarPersona = new JButton("Registrar");
+		btnRegistrarPersona.setBounds(351, 375, 89, 23);
+		contentPane.add(btnRegistrarPersona);
+		
+		JDateChooser dateChooserNacimiento = new JDateChooser();
+		dateChooserNacimiento.setBounds(229, 237, 163, 20);
+		contentPane.add(dateChooserNacimiento);
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-	}
-
 }
